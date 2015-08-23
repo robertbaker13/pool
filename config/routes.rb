c2e018293f2d8ebe7180f67ea2a082adb883ccdd
Bobby Baker <email": null>
2015-08-22 18:11:08 -0700

    add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  # root 'welcome#index'
+
+  resources :employees do
+    resources :dispersements
+  end
+
+
+  # resources :positions
+  # resources :schedule_types
+  # resources :schedules do
+  #   resources :tip_pools
+  # end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
