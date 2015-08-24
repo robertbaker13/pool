980d990e8094d7d061576ec3f1b13cb113d4e698
Bobby Baker <email": null>
2015-08-24 11:36:41 -0700

    rename tip pool employee position model

---
+++ app/models/tip_pool_employee_position.rb
@@ -0,0 +1,9 @@
+class TipPoolEmployeePosition < ActiveRecord::Base
+  belongs_to :employee_position
+  belongs_to :tip_pool, dependent: :destroy
+  has_many :dispersements
+
+  def dispersed
+    self.tip_pool.dispersed
+  end
+end
