516ccc338ecd89efe4accb0157cd0998110e2d98
Bobby Baker <email": null>
2015-08-24 11:37:19 -0700

    remove old pools files

--- app/models/pool_employee_position.rb
+++
@@ -1,9 +0,0 @@
-class PoolEmployeePosition < ActiveRecord::Base
-  belongs_to :employee_position
-  belongs_to :tip_pool, dependent: :destroy
-  has_many :dispersements
-
-  def dispersed
-    self.tip_pool.dispersed
-  end
-end
